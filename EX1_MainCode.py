e3e98c86f81c5597044431e81bca8be632118ea7
Shay Saraf <shaysaraf08@gmail.com>
2025-03-30 11:58:59 +0300

    finished, need to work on a better printing

--- EX1_MainCode.py
+++ EX1_MainCode.py
@@ -5,7 +5,6 @@ import torchvision
 import torchvision.transforms as transforms
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
-import numpy as np
 
 # Define the LeNet-5 model with Dropout
 class LeNet5Dropout(nn.Module):
@@ -13,7 +12,7 @@ class LeNet5Dropout(nn.Module):
         super(LeNet5Dropout, self).__init__()
         self.conv1 = nn.Conv2d(1, 6, kernel_size=5)
         self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
-        self.conv3 = nn.Conv2d(16, 120, kernel_size=4)  # Adjusted kernel size to 4
+        self.conv3 = nn.Conv2d(16, 120, kernel_size=4)
         self.fc1 = nn.Linear(120, 84)
         self.fc2 = nn.Linear(84, 10)
         self.dropout = nn.Dropout(p=0.5)
@@ -24,22 +23,80 @@ class LeNet5Dropout(nn.Module):
         x = F.relu(self.conv2(x))
         x = F.max_pool2d(x, 2)
         x = F.relu(self.conv3(x))
-        x = F.avg_pool2d(x, 1)  # Adjusted pooling size to 1
+        x = F.avg_pool2d(x, 1)
         x = x.view(-1, 120)
         x = self.dropout(F.relu(self.fc1(x)))
         x = self.fc2(x)
         return x
 
-# Plot the convergence graph
-def plot_convergence(train_accuracies, test_accuracies, title):
-    epochs = range(1, len(train_accuracies) + 1)
-    plt.plot(epochs, train_accuracies, 'b', label='Train accuracy')
-    plt.plot(epochs, test_accuracies, 'r', label='Test accuracy')
-    plt.title(title)
-    plt.xlabel('Epochs')
-    plt.ylabel('Accuracy')
-    plt.legend()
-    plt.show()
+# Define the LeNet-5 model with Weight Decay
+class LeNet5WeightDecay(nn.Module):
+    def __init__(self):
+        super(LeNet5WeightDecay, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=5)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.conv3 = nn.Conv2d(16, 120, kernel_size=4)
+        self.fc1 = nn.Linear(120, 84)
+        self.fc2 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv3(x))
+        x = F.avg_pool2d(x, 1)
+        x = x.view(-1, 120)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+# Define the LeNet-5 model with Batch Normalization
+class LeNet5BatchNorm(nn.Module):
+    def __init__(self):
+        super(LeNet5BatchNorm, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=5)
+        self.bn1 = nn.BatchNorm2d(6)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.bn2 = nn.BatchNorm2d(16)
+        self.conv3 = nn.Conv2d(16, 120, kernel_size=4)
+        self.bn3 = nn.BatchNorm2d(120)
+        self.fc1 = nn.Linear(120, 84)
+        self.fc2 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.avg_pool2d(x, 1)
+        x = x.view(-1, 120)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+# Define the LeNet-5 model without Regularization
+class LeNet5(nn.Module):
+    def __init__(self):
+        super(LeNet5, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=5)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.conv3 = nn.Conv2d(16, 120, kernel_size=4)
+        self.fc1 = nn.Linear(120, 84)
+        self.fc2 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv3(x))
+        x = F.avg_pool2d(x, 1)
+        x = x.view(-1, 120)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
 
 # Training and evaluation function
 def train_and_evaluate(model, train_loader, test_loader, criterion, optimizer, num_epochs=10):
@@ -81,6 +138,29 @@ def train_and_evaluate(model, train_loader, test_loader, criterion, optimizer, n
 
     return train_accuracies, test_accuracies
 
+# Plot the convergence graph
+def plot_convergence(train_accuracies, test_accuracies, title):
+    epochs = range(1, len(train_accuracies) + 1)
+    plt.plot(epochs, train_accuracies, 'b', label='Train accuracy')
+    plt.plot(epochs, test_accuracies, 'r', label='Test accuracy')
+    plt.title(title)
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.show()
+
+# Plot the results table
+def plot_results_table(results):
+    fig, ax = plt.subplots()
+    ax.axis('tight')
+    ax.axis('off')
+    table_data = [["Model", "Train Accuracy", "Test Accuracy"]] + results
+    table = ax.table(cellText=table_data, cellLoc='center', loc='center')
+    table.auto_set_font_size(False)
+    table.set_fontsize(10)
+    table.scale(1.5, 1.5)
+    plt.show()
+
 # Example usage
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
 train_dataset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
@@ -88,15 +168,44 @@ test_dataset = torchvision.datasets.MNIST(root='./data', train=False, download=T
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=False)
 
-# Initialize the network
-net = LeNet5Dropout()
+# Initialize the networks
+net_dropout = LeNet5Dropout()
+net_weight_decay = LeNet5WeightDecay()
+net_batch_norm = LeNet5BatchNorm()
+net_no_reg = LeNet5()
 
-# Define the loss function and optimizer
+# Define the loss function
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9, weight_decay=0.0005)
 
-# Train and evaluate the model
-train_accuracies, test_accuracies = train_and_evaluate(net, train_loader, test_loader, criterion, optimizer, num_epochs=10)
+# Define the optimizers
+optimizer_dropout = optim.SGD(net_dropout.parameters(), lr=0.01, momentum=0.9, weight_decay=0.0005)
+optimizer_weight_decay = optim.SGD(net_weight_decay.parameters(), lr=0.01, momentum=0.9, weight_decay=0.0005)
+optimizer_batch_norm = optim.SGD(net_batch_norm.parameters(), lr=0.01, momentum=0.9, weight_decay=0.0005)
+optimizer_no_reg = optim.SGD(net_no_reg.parameters(), lr=0.01, momentum=0.9, weight_decay=0.0005)
 
-# Plot the convergence graph
-plot_convergence(train_accuracies, test_accuracies, 'LeNet-5 with Dropout')
+# Train and evaluate the models
+train_accuracies_dropout, test_accuracies_dropout = train_and_evaluate(net_dropout, train_loader, test_loader, criterion, optimizer_dropout, num_epochs=10)
+train_accuracies_weight_decay, test_accuracies_weight_decay = train_and_evaluate(net_weight_decay, train_loader, test_loader, criterion, optimizer_weight_decay, num_epochs=10)
+train_accuracies_batch_norm, test_accuracies_batch_norm = train_and_evaluate(net_batch_norm, train_loader, test_loader, criterion, optimizer_batch_norm, num_epochs=10)
+train_accuracies_no_reg, test_accuracies_no_reg = train_and_evaluate(net_no_reg, train_loader, test_loader, criterion, optimizer_no_reg, num_epochs=10)
+
+# Plot the convergence graphs
+plot_convergence(train_accuracies_dropout, test_accuracies_dropout, 'LeNet-5 with Dropout')
+plot_convergence(train_accuracies_weight_decay, test_accuracies_weight_decay, 'LeNet-5 with Weight Decay')
+plot_convergence(train_accuracies_batch_norm, test_accuracies_batch_norm, 'LeNet-5 with Batch Normalization')
+plot_convergence(train_accuracies_no_reg, test_accuracies_no_reg, 'LeNet-5 without Regularization')
+
+# Collect results
+results = [
+    ["LeNet-5 with Dropout", train_accuracies_dropout[-1], test_accuracies_dropout[-1]],
+    ["LeNet-5 with Weight Decay", train_accuracies_weight_decay[-1], test_accuracies_weight_decay[-1]],
+    ["LeNet-5 with Batch Norm", train_accuracies_batch_norm[-1], test_accuracies_batch_norm[-1]],
+    ["LeNet-5 without Reg", train_accuracies_no_reg[-1], test_accuracies_no_reg[-1]]
+]
+
+# Plot the results table
+plot_results_table(results)
+
+# Determine the winner
+best_model = max(results, key=lambda x: x[2])
+print(f"The best model is {best_model[0]} with a test accuracy of {best_model[2]:.2f}%")
